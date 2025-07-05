9ac30060e3e9e9aa1aab67ced9b9bd814f7c5ad4
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2025-07-05 15:21:32 +0900

    [add]Courseクラスの実装

---
+++ src/com/github/tanakakfuji/hrcui/course/Course.java
@@ -0,0 +1,48 @@
+package com.github.tanakakfuji.hrcui.course;
+
+import com.github.tanakakfuji.hrcui.enums.WeatherType;
+
+import java.util.Random;
+
+public class Course {
+    private String name;
+    private int length;
+    private boolean isDirtTrack;
+    private WeatherType weather;
+    private final int MIN_COURSE_LENGTH = 1000;
+    private final int MAX_COURSE_LENGTH = 3600;
+
+    public Course(String name) {
+        this.name = name;
+        Random rand = new Random();
+        int lr = rand.nextInt(MIN_COURSE_LENGTH / 100, MAX_COURSE_LENGTH / 100 + 1);
+        this.length = lr * 100;
+
+        int dr = rand.nextInt(2);
+        this.isDirtTrack = dr == 0;
+
+        int wr = rand.nextInt(3);
+        this.weather = switch (wr) {
+            case 0 -> WeatherType.SUNNY;
+            case 1 -> WeatherType.CLOUDY;
+            case 2 -> WeatherType.RAINY;
+            default -> throw new IllegalStateException("予期しない値: " + wr);
+        };
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public boolean getIsDirtTrack() {
+        return isDirtTrack;
+    }
+
+    public WeatherType getWeather() {
+        return weather;
+    }
+}
