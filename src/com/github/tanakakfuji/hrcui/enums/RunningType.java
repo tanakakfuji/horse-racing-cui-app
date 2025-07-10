0fb4d78ff151ee8c21d8a1794755b5e0105f39a7
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2025-07-10 15:30:09 +0900

    [add]RunningTypeとWeatherTypeにnameフィールドの追加

--- src/com/github/tanakakfuji/hrcui/enums/RunningType.java
+++ src/com/github/tanakakfuji/hrcui/enums/RunningType.java
@@ -1,5 +1,18 @@
 package com.github.tanakakfuji.hrcui.enums;
 
 public enum RunningType {
-    FRONT_RUNNER, STALKER, LOOKER, CLOSER
+    FRONT_RUNNER("逃げ"),
+    STALKER("先行"),
+    LOOKER("差し"),
+    CLOSER("追い込み");
+
+    String name;
+
+    RunningType(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
 }
--- src/com/github/tanakakfuji/hrcui/enums/WeatherType.java
+++ src/com/github/tanakakfuji/hrcui/enums/WeatherType.java
@@ -1,5 +1,17 @@
 package com.github.tanakakfuji.hrcui.enums;
 
 public enum WeatherType {
-    SUNNY, CLOUDY, RAINY
+    SUNNY("晴れ"),
+    CLOUDY("曇り"),
+    RAINY("雨");
+
+    String name;
+
+    WeatherType(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
 }
