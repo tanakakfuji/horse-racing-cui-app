c8f1bb25ea004a5dc11cf7e6151b5c8c2d5728d3
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2025-07-12 12:04:18 +0900

    [add]Raceクラスの実装

--- src/com/github/tanakakfuji/hrcui/race/Race.java
+++ src/com/github/tanakakfuji/hrcui/race/Race.java
@@ -1,7 +1,46 @@
 package com.github.tanakakfuji.hrcui.race;
 
-public class Race {
-    public Race(int numOfHorses) {
+import com.github.tanakakfuji.hrcui.course.Course;
+import com.github.tanakakfuji.hrcui.factory.CourseFactory;
+import com.github.tanakakfuji.hrcui.factory.RacehorseFactory;
+import com.github.tanakakfuji.hrcui.horse.Racehorse;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Race {
+    private Course course;
+    private Map<Racehorse, Double> racehorsePositionMap = new LinkedHashMap<>();
+    private Racehorse[] ranking;
+
+    public Race(int numOfHorses) {
+        this.course = CourseFactory.createCourse();
+        setRacehorses(numOfHorses);
+        this.ranking = new Racehorse[numOfHorses];
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public Racehorse[] getRacehorses() {
+        return racehorsePositionMap.keySet().toArray(new Racehorse[0]);
+    }
+
+    public Racehorse[] getRanking() {
+        return ranking;
+    }
+
+    private void setRacehorses(int numOfHorses) {
+        for (int i = 0; i < numOfHorses; i++) {
+            Racehorse createdRacehorse;
+            while (true) {
+                createdRacehorse = RacehorseFactory.createRacehorse(i + 1);
+                if (!racehorsePositionMap.containsKey(createdRacehorse)) {
+                    break;
+                }
+            }
+            racehorsePositionMap.put(createdRacehorse, 0.0);
+        }
     }
 }
