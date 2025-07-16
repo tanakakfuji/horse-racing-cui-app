54afc2e3923cab2f14aed66270078c5ebf152cd8
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2025-07-16 10:17:29 +0900

    [add]ConditionTypeにspeedフィールドを追加

--- src/com/github/tanakakfuji/hrcui/enums/ConditionType.java
+++ src/com/github/tanakakfuji/hrcui/enums/ConditionType.java
@@ -1,5 +1,19 @@
 package com.github.tanakakfuji.hrcui.enums;
 
 public enum ConditionType {
-    PERFECT, FINE, NORMAL, WEAK, AWFUL
+    PERFECT(0.4),
+    FINE(0.2),
+    NORMAL(0.0),
+    WEAK(-0.2),
+    AWFUL(-0.4);
+
+    double speed;
+
+    ConditionType(double speed) {
+        this.speed = speed;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
 }
